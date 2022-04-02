a055dfe9810b5e9027b70ab501177c1fbcdfa219
Eshaan Barkataki <eshaanbarkataki@gmail.com>
2022-04-02 09:31:47 -0400

    OCR multiline

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -6,6 +6,11 @@ from flask import Flask, flash, request, redirect, url_for, render_template, Blu
 from werkzeug.utils import secure_filename
 from matplotlib.pyplot import imread
 from PIL import Image
+import torch
+import logging
+
+# make sure you delete this later during production
+logging.basicConfig(level=logging.DEBUG)
 
 processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
 model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
@@ -22,8 +27,28 @@ def allowed_file(filename):
 def OCR(filename):
     image = Image.open(os.path.join('static', filename)).convert('RGB')
     pixel_values = processor(images=image, return_tensors='pt').pixel_values
-    generated_ids = model.generate(pixel_values)
-    return processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    horiz_hist = (torch.sum(torch.sum(pixel_values, 1), 2)/255).reshape(384)
+    linebreak_idx = [0]
+    check_margin_end = False
+    passage = []
+    print(f"horiz list: {horiz_hist}")
+    for ix,sum in enumerate(horiz_hist):
+        if sum <= 800 and check_margin_end == False:
+            linebreak_idx.append(ix)
+            check_margin_end =  True
+            # generated_ids = model.generate(pixel_values)
+            # return processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        if sum >= 800 and check_margin_end == True:
+            top_current = linebreak_idx.pop()
+            lower = linebreak_idx[0]
+            average = (ix+top_current)/2
+            cropped_image = image.crop((0, lower, image.size[0], average))
+            pixel_values = processor(images=cropped_image, return_tensors='pt').pixel_values
+            generated_ids = model.generate(pixel_values)
+            passage.append(processor.batch_decode(generated_ids, skip_special_tokens=True)[0])
+            check_margin_end = False
+
+    return " ".join(passage)
 
 def generateQuestions(text):
     PATH = "./results/checkpoint-17500"
@@ -59,5 +84,3 @@ def upload_image():
 # @Routes.route('/display/<filename>')
 # def display_image(filename):
 #     return redirect(url_for('static', filename='uploads/'+filename), code=301)
-
-
