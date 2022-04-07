99cea58b30ee7cb70db798365515e3ffb03cb100
Vis <vt201916384@gmail.com>
2022-04-07 19:29:46 -0400

    fixed OCR

--- backend/routes.py
+++ backend/routes.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 from PIL import Image
 import torch
 import logging
+import numpy as np
 
 # make sure you delete this later during production
 logging.basicConfig(level=logging.DEBUG)
@@ -22,34 +23,39 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# def get_split_values(pixel_values):
-#     horiz_hist = (torch.sum(torch.sum(pixel_values, 1), 2)/255).reshape(384)
-#     linebreak_idx = [0]
-
+def get_split_values(image):
+    arr = np.mean(np.array(image), (2,1))
+    linebreak_idx = [0]
+    indexes = []
+    check_margin_end=True
+    for ix,sum in enumerate(arr):
+        if sum<=225 and check_margin_end==True:
+            linebreak_idx.append(ix)
+            check_margin_end==False
+        if sum>=225 and check_margin_end==False:
+            linebreak_idx.append(ix)
+            indexes.append(linebreak_idx)
+            linebreak_idx = []
+            check_margin_end=True
+    
+    return indexes
+        
 
 
 def OCR(filename):
     image = Image.open(os.path.join('static', filename)).convert('RGB')
-    pixel_values = processor(images=image, return_tensors='pt').pixel_values
-    horiz_hist = (torch.sum(torch.sum(pixel_values, 1), 2)/255).reshape(384)
-    linebreak_idx = [0]
-    check_margin_end = False
-    passage = []
-    for ix,sum in enumerate(horiz_hist):
-        if sum <= 3.13 and check_margin_end == False:
-            linebreak_idx.append(ix)
-            check_margin_end =  True
-            # generated_ids = model.generate(pixel_values)
-            # return processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        if sum >= 3.13 and check_margin_end == True:
-            top_current = linebreak_idx.pop()
-            lower = linebreak_idx[0]
-            average = (ix+top_current)/2
-            cropped_image = image.crop((0, lower, image.size[0], average))
-            pixel_values = processor(images=cropped_image, return_tensors='pt').pixel_values
-            generated_ids = model.generate(pixel_values)
-            passage.append(processor.batch_decode(generated_ids, skip_special_tokens=True)[0])
-            check_margin_end = False
+    idxs = get_split_values(image)
+    passage = ""
+
+    for idx in idxs:
+        cropped_image = image.crop((0, idx[0], image.size[0], idx[1]))
+        pixel_values = processor(images=cropped_image, return_tensors="pt").pixel_values
+        generated_ids = model.generate(pixel_values)
+        passage.append(processor.batch_decode(generated_ids, skip_special_tokens=True)[0])
+    
+    return passage
+
+
 
     return " ".join(passage)
 
@@ -85,3 +91,24 @@ def upload_image():
         print("Generating Questions:")
         questions = generateQuestions(OCR_output)
         return {"question" : questions, "OCR_output" : OCR_output, "error" : ""}
+
+
+
+    # pixel_values = processor(images=image, return_tensors='pt').pixel_values
+    # horiz_hist = (torch.sum(torch.sum(pixel_values, 0), 0)/255).reshape(384)
+    # linebreak_idx = [0]
+    # check_margin_end = False
+    # passage = []
+    # for ix,sum in enumerate(horiz_hist):
+    #     if sum <= 3.13 and check_margin_end == False:
+    #         linebreak_idx.append(ix)
+    #         check_margin_end =  True
+    #     if sum >= 3.13 and check_margin_end == True:
+    #         top_current = linebreak_idx.pop()
+    #         lower = ((linebreak_idx[0])/384)*image.size[1]
+    #         average = (((ix+top_current)/2)/384)*image.size[1]
+    #         cropped_image = image.crop((0, lower, image.size[0], average))
+    #         pixel_values = processor(images=cropped_image, return_tensors='pt').pixel_values
+    #         generated_ids = model.generate(pixel_values)
+    #         passage.append(processor.batch_decode(generated_ids, skip_special_tokens=True)[0])
+    #         check_margin_end = False
